d66cdb85e9fe5e926c6e878daa6bb00f5a58520c
Malcolm Still <malcolm.still@gmail.com>
2022-11-02 21:52:13 +0000

    Print help

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,45 @@
 const std = @import("std");
+const io = std.io;
+const mem = std.mem;
+const process = std.process;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout_file = io.getStdOut().writer();
+    var bw = io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var it = process.args();
+
+    _ = it.next(); // Skip "clerk"
+
+    const command = it.next() orelse {
+        try printVersion(stdout);
+        try stdout.print("Expected <command>\n\n", .{});
+        try stdout.print("Commands:\n", .{});
+        try printHelp(stdout);
+        try bw.flush(); // don't forget to flush!
+        process.exit(1);
+    };
+
+    if (mem.eql(u8, command, "add")) {
+        //
+    }
 
     try bw.flush(); // don't forget to flush!
+}
+
+fn printHelp(stdout: anytype) !void {
+    try stdout.print("\n", .{});
+    try stdout.print("\tadd\tAdd a new note to the database\n", .{});
+    try stdout.print("\tdone\tMark note as done\n", .{});
+    try stdout.print("\n", .{});
+}
+
+fn printVersion(stdout: anytype) !void {
+    try stdout.print("clerk v0.0.1\n\n", .{});
 }
 
 test "simple test" {
