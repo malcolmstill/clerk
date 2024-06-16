a3459232f4d5e496ca62ea6f8fffc6c72223e58e
Malcolm Still <malcolm.still@gmail.com>
2024-06-16 13:57:35 +0100

    Update to zig 0.13.0 (#37)
    * Update to zig 0.13.0
    
    - Remove zig-sqlite from vendored deps...use build.zig.zon instead
    - ansi-term isn't quite up to zig-0.13.0 yet so keep it vendored
    
    * Update GitHub Actions

--- lib/ansi-term/README.md
+++ lib/ansi-term/README.md
@@ -7,3 +7,11 @@ Zig library for dealing with ANSI Terminals (escape codes, styles, etc.)
 This was originally code which was extracted from
 [lscolors](https://github.com/ziglibs/lscolors) for use in
 other zig libraries. More features have been added since.
+
+`ansi-term` is designed to work with Zig 0.12.0.
+
+## Documentation
+
+Automatically generated documentation for the project
+can be found at https://ziglibs.github.io/ansi-term/.
+Note that autodoc is currently in beta; the website may be broken or incomplete.
--- lib/ansi-term/build.zig
+++ lib/ansi-term/build.zig
@@ -1,13 +1,30 @@
-const Builder = @import("std").build.Builder;
+const Build = @import("std").Build;
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    var main_tests = b.addTest("src/main.zig");
-    main_tests.setTarget(target);
-    main_tests.setBuildMode(mode);
+    _ = b.addModule("ansi-term", .{
+        .root_source_file = b.path("src/main.zig"),
+    });
+
+    var main_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_main_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
+
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = main_tests.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&install_docs.step);
 }
